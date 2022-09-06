19797922301944e6e9c3329d8152b5fc39224250
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-09-06 21:58:19 +0000

    slay

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,18 @@
+#part 1
+print(10 * 5)
+print(10**2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15) #it goes on forever
+
+#part 2
+rate = int(input("What is the current exchange rate for the Euro to Dollar?"))
+amount = int(input("How much currency would you like to exchange?"))
+total = rate*amount
+result = total-3
+print("Your result is $", result)
