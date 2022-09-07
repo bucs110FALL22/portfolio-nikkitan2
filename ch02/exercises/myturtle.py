7168e6f0fa87eefb6cb9789d2ff935a78321eec8
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-09-07 22:38:02 +0000

    slay

--- ch02/exercises/myturtle.py
+++ ch02/exercises/myturtle.py
@@ -1,1 +1,19 @@
 import turtle
+
+my_turtle = turtle.Turtle()
+window = turtle.Screen()
+
+my_turtle.shape("turtle")
+my_turtle.color("purple")
+
+
+length = 50
+angle = 90
+
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
