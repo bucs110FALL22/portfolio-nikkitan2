48d6ad49a1d0adb2ee5bf6e0daa3c1529f916de6
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-09-09 18:32:01 +0000

    slay

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,13 +1,17 @@
 import random
 #Part A
 classes_per_week = 3
+print("Classes per week:", classes_per_week)
 weeks = 16
+print("Number of weeks:", weeks)
 classes = 5
+print("Number of classes:", classes)
 tuition = 6000
+print("Cost of tuition:", tuition)
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 cost_per_class = (cost_per_week / classes_per_week)
 print("Cost per class:", cost_per_class)
 #Part B
-mylist = [1, 2, 3, 4, 5]
-print(random.choice(mylist))
+numbers = [1, 2, 3, 4, 5]
+print(random.choice(numbers))
