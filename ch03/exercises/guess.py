26b6c07aa7ca77e9f0184c49ecaae8a34d4d9a50
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-09-19 21:12:42 +0000

    guess.py

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,17 +1,20 @@
 import random
 
-num = random.randrange(1.11)
-num_guesses = 1
+num = random.randrange(1,11)
+num_guesses = 0
+correct_guess = False
 
-for i in range(3):
-  guess_num = int(input("Please enter a guess"))
-  #num_guesses = num_guesses +1
-  num_guesses += 1
-  if guess_num > num:
-    print("Your guess is too high")
-  elif guess_num < num:
-    print("Your guess is too low")
-  else:
-    print("Correct!")
+for i in range(5):
+  if not correct_guess: #flag variable
+    guess_num = int(input("Please enter a guess"))
+    #num_guesses = num_guesses + 1
+    num_guesses += 1
+    if guess_num > num:
+      print("Your guess is too high")
+    elif guess_num < num:
+      print("Your guess is too low")
+    else:
+      print("Correct!")
+      correct_guess = True
 
 print("it took you", num_guesses, "to get it right.")
