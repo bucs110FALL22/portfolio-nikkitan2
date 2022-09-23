52a17df5b8db84ab21f2ed5b927357a0332b8c31
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-09-23 14:38:23 +0000

    idk

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -26,11 +26,28 @@ leonardo.forward(50)
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 #Race 2
+for i in range(10):
+  michelangelo.forward(x)
+  leonardo.forward(x)
 
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
 # PART B - complete part B here
 import pygame
 import math
+
+cords = [(x1,y1),(x2,y2),(x3,y3)]
+num_sides = 3
+side_length = 20
+offset =
+
+for i in range():
+  theta = (2.0 * math.pi * (loop increment variable))) / num_sides
+  x = side_length * math.cos(theta) + offset
+  y = side_length * math.sin(thetha) + offset
+
+pygame.draw.polygon(cords)
 
 pygame.init()
 window = pygame.display.set_mode()
