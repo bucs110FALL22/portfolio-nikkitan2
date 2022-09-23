e51bea650f0eb6a9d0f86c9d38a8c7a428751d26
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-09-23 19:09:00 +0000

    slayy

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -18,38 +18,131 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
-x = random.randrange(1,10)
+
+x = random.randrange(0,11)
 print(x)
 #Race 1
+michelangelo.speed(1)
+leonardo.speed(1)
 michelangelo.forward(50)
 leonardo.forward(50)
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 #Race 2
 for i in range(10):
+  michelangelo.speed(1)
+  leonardo.speed(1)
   michelangelo.forward(x)
   leonardo.forward(x)
 
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
+michelangelo.clear()
+leonardo.clear()
+
 # PART B - complete part B here
 import pygame
 import math
-
-cords = [(x1,y1),(x2,y2),(x3,y3)]
-num_sides = 3
-side_length = 20
-offset =
-
-for i in range():
-  theta = (2.0 * math.pi * (loop increment variable))) / num_sides
-  x = side_length * math.cos(theta) + offset
-  y = side_length * math.sin(thetha) + offset
-
-pygame.draw.polygon(cords)
-
 pygame.init()
-window = pygame.display.set_mode()
 
+window = pygame.display.set_mode()
+window.fill('ghostwhite')
+pygame.display.flip()
+
+coords = []
+
+#equilateral triangle
+num_sides = 3
+side_length = 50
+offset = 100
+
+for i in range(num_sides):
+  theta = (2.0 * math.pi * (i)) / num_sides
+  x = side_length * math.cos(theta) + offset
+  y = side_length * math.sin(theta) + offset
+  coords.append([x,y])
+
+window.fill('ghostwhite')
+pygame.display.flip()
+
+pygame.draw.polygon(window,"blue",coords)
+pygame.display.flip()
+pygame.time.wait(2000)
+
+window.fill("ghostwhite")
+pygame.display.flip()
+
+
+#square
+coords = []
+num_sides = 4
+
+for i in range(num_sides):
+  theta = (2.0 * math.pi * (i)) / num_sides
+  x = side_length * math.cos(theta) + offset
+  y = side_length * math.sin(theta) + offset
+  coords.append([x,y])
+
+
+pygame.draw.polygon(window,"blue",coords)
+pygame.display.flip()
+pygame.time.wait(2000)
+
+window.fill("ghostwhite")
+pygame.display.flip()
+
+#hexagon
+coords = []
+num_sides = 6
+
+for i in range(num_sides):
+  theta = (2.0 * math.pi * (i)) / num_sides
+  x = side_length * math.cos(theta) + offset
+  y = side_length * math.sin(theta) + offset
+  coords.append([x,y])
+
+
+pygame.draw.polygon(window,"blue",coords)
+pygame.display.flip()
+pygame.time.wait(2000)
+
+window.fill("ghostwhite")
+pygame.display.flip()
+
+#nonagon
+coords = []
+num_sides = 9
+
+for i in range(num_sides):
+  theta = (2.0 * math.pi * (i)) / num_sides
+  x = side_length * math.cos(theta) + offset
+  y = side_length * math.sin(theta) + offset
+  coords.append([x,y])
+
+
+pygame.draw.polygon(window,"blue",coords)
+pygame.display.flip()
+pygame.time.wait(2000)
+
+#circle-ish
+coords = []
+num_sides = 360
+
+for i in range(num_sides):
+  theta = (2.0 * math.pi * (i)) / num_sides
+  x = side_length * math.cos(theta) + offset
+  y = side_length * math.sin(theta) + offset
+  coords.append([x,y])
+
+
+pygame.draw.polygon(window,"blue",coords)
+pygame.display.flip()
+pygame.time.wait(2000)
+
+window.fill("ghostwhite")
+pygame.display.flip()
+
+window.fill("ghostwhite")
+pygame.display.flip()
 window.exitonclick()
