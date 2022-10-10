4c854916eb70afeb575dfbdbf84734c410a5b5d6
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-10-10 22:06:15 +0000

    no

---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,16 @@
+def star_pyramid():
+  stars = int(input("Please enter the number of rows you would like."))
+  while stars != 0:
+    print(stars*"*")
+    stars 
+
+star_pyramid()
+
+def rstar_pyramid():
+ stars = int(input("Please enter the number of rows you would like."))
+ while stars != 0:
+    print(stars*"*")
+    stars -=1
+
+rstar_pyramid()
+
