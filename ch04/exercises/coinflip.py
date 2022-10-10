a1890bf07c038b4d6a848b40c8eef385c76fbd3d
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-10-10 21:14:10 +0000

    yur

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,15 +1,39 @@
-import turtle
 import random
+import turtle
 
-my_turtle = turtle.Turtle()
-window = turtle.Screen()
-window.bgcolor("white")
-my_turtle.shape("turtle")
-my_turtle.color("pink")
+def isinscreen(window, turt):
+  turtleX = t.xcor()
+  turtleY = t.ycor()
+ 
+  x_range = wn.window_width()/2
+  y_range = wn.window_height()/2
 
+  if abs(turtleX) > x_range or abs(turtleY) > y_range:
+    return False
+  return True
 
-def cointoss():
-  return random.choice(["heads","tails"])
-  side = random.randint("heads","tails")
-  if (side = "heads"):
-    my_turtule
+t = turtle.Turtle()
+wn = turtle.Screen()
+t.shape('turtle')
+t.speed(0)
+
+distance = 10
+angle = 90
+is_in_screen = True
+
+colors = ["green", "blue", "purple"]
+
+while is_in_screen:
+  coin = random.randrange(0,2)
+  if coin == 0:
+    t.left(angle)
+  else:
+    t.right(angle)
+  t.forward(distance)
+  
+  t.color(colors[0])
+  colors.append(colors.pop(0))
+  
+
+wn.bgcolor("yellow")
+wn.exitonclick
