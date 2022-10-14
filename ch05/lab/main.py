07a87dac5803e5ab1066a6f1a77035a684822876
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-10-14 19:21:24 +0000

    ugh

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,17 +1,24 @@
 
 def seq3np1(n):
   n = 101
-  count = 0
+  
+  upper_limit = 20
+  iters = {}
+  
+  for i in range(2, upper_limit + 1):
+    count = 0
   while n > 1:
     print(n)
     if (n % 2) == 0:
-      n = n // 2 
+      n = n // 2     
     else:
       n = 3 * n + 1
     count = count + 1
+    iters[n] = (count)
       
     print(n)
     print(count)
+    print(iters)
   
   
 seq3np1(3)
