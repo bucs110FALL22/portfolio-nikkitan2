9bad422692a261976f5fd33130d5975016a002ea
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-10-18 02:33:54 +0000

    uh

---
+++ ch05/exercises/boolean_functions.py
@@ -0,0 +1,12 @@
+def percentage_to_letter(score=0):
+  score = int(input("What was your exam percentage?")
+  if score >= 90:
+    print("You received an A")
+  if score 80-<90:
+    print("You received a B")
+  if score 70-<80:
+    print(`"You received a C")
+  if score 60-<70:
+    print("You received a D")
+  if score < 60:
+    grade = print("You received a F")
