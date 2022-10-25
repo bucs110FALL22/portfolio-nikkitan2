cc41b6117dbcf3b0362cace088a32d065fe895a0
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-10-25 01:01:02 +0000

    :(

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,16 @@
+class Player:
+  def __init__(self):
+    self.player_num = 1
+    self.health = 0
+    self.dodge = 0
+
+
+class Position:
+  def __init__(self):
+    self.move = 10
+    self.direction = 90
+    self.speed = 20
+
+class :
+  def __init__(self):
+    
