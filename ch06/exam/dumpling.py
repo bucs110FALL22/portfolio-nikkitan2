57fa562bf1bba3a02dfddc6c4f9e00b2ce43edd2
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-10-28 21:40:44 +0000

    :)

--- ch06/exam/dumpling.py
+++ ch06/exam/dumpling.py
@@ -1,9 +1,42 @@
 import turtle
-import math
+import pygame
 
-dumpling = turtle.Turtle()
+t = turtle.Turtle()
 window = turtle.Screen()
-window.bgcolor("yellow")
-dumpling.color("papayawhip")
-dumpling.speed(1)
+window.bgcolor("black")
+t.color("white")
+t.speed(1)
+t.pensize(10)
 
+def draw(r):
+  for i in range(2):
+    t.circle(r,90)      
+    t.circle(r//2,90)
+t.seth(-45)
+
+t.fillcolor("white")
+t.begin_fill()
+draw(100)
+t.end_fill()
+t.begin_fill()
+t.penup()
+t.goto(20,100)
+t.pendown()
+draw(20)
+t.end_fill()
+
+t.begin_fill()
+t.penup()
+t.goto(50,110)
+t.pendown()
+draw(25)
+t.end_fill()
+
+t.begin_fill()
+t.penup()
+t.goto(90,100)
+t.pendown()
+draw(20)
+t.end_fill()
+
+pygame.time.wait(2000)
