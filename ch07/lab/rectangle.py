95e232732f5248ba5e303333d8f83a1078735751
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-11-04 18:34:46 +0000

    :)

--- ch07/lab/rectangle.py
+++ ch07/lab/rectangle.py
@@ -5,5 +5,15 @@ class Rectangle:
     self.height = max(h, 0)
     self.width = max(w, 0)
   def __str__(self):
-    s = "(x: {}, y: {) width: {}, height: {}"
-    return s.format(self.x, self.y, self.width, self.height)
+    result_str = f"coordinate: {self.x, self.y}"
+    result_str += f"\nheight: {self.height}"
+    result_str += f"\nwidth: {self.width}"
+    return result_str
+    
+    
+
+
+def main():
+  rec_size = Rectangle(1, 5, 7, 10)
+  print(rec_size)
+main()
