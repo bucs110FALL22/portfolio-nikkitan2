1238c2c8e4b7eedafb86f07b135da745c56f3277
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-11-04 18:42:58 +0000

    :0

--- ch07/lab/surface.py
+++ ch07/lab/surface.py
@@ -1,13 +1,13 @@
-import Rectangle
+import rectangle
 
 class Surface:
   def __init__(self, filename, x, y, h, w):
-    self.rect = Rectangle.Rectangle(x, y, h, w) 
+    self.rect = rectangle.Rectangle(x, y, h, w) 
     self.image = filename
 
   def getRect(self):
     return self.rect
     
 sur = Surface("filename", 5, -7, 10, 10)
-rectangle = sur.getRect()
-print(rectangle)
+rec = sur.getRect()
+print(rec)
