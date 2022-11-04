b2c1ed746b10dfca4203af0363ebac47b95b5992
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-11-04 19:04:27 +0000

    :0

--- ch07/lab/main.py
+++ ch07/lab/main.py
@@ -1,30 +1,21 @@
-import Rectangle
-import Surface
+from rectangle import Rectangle
+from surface import Surface
 
-#Rectangle class
-r = Rectangle(10, 10, 10, 10)
-assert((r.x, r.y, r.height, r.width) == (10,10,10,10))
-
-r = Rectangle(-1, 1, 1, 1)
-assert((r.x, r.y, r.height, r.width) == (0,1,1,1))
-
-r = Rectangle(1, -1, 1, 1)
-import rectangle
-import surface
-
-#Rectangle class
-assert((r.x, r.y, r.height, r.width) == (1,0,1,1))
-
-r = Rectangle(1, 1, -1, 1)
-assert((r.x, r.y, r.height, r.width) == (1,1,0,1))
-
-r = Rectangle(1, 1, 1, -1)
-assert((r.x, r.y, r.height, r.width) == (1,1,1,0))
-
-#Surface class
-s = Surface("myimage.png", 10, 10, 10, 10)
-assert((s.rect.x, s.rect.y, s.rect.height, s.rect.width) == (10,10,10,10)
-s = s.getRect()
-assert(srect.x,  s.getRect().y, srect.height,  srect.width) == (10,10,10,10)
-assert s.image 
-print("Test Complete!")
+def main():
+  r = Rectangle(10, 10, 10, 10)
+  assert((r.x, r.y, r.height, r.width) == (10,10,10,10))
+  r = Rectangle(-1, 1, 1, 1)
+  assert((r.x, r.y, r.height, r.width) == (0,1,1,1))
+  r = Rectangle(1, -1, 1, 1)
+  assert((r.x, r.y, r.height, r.width) == (1,0,1,1))
+  r = Rectangle(1, 1, -1, 1)
+  assert((r.x, r.y, r.height, r.width) == (1,1,0,1))
+  r = Rectangle(1, 1, 1, -1)
+  assert((r.x, r.y, r.height, r.width) == (1,1,1,0))
+  print(r)
+  s = Surface("myimage.png", 10, 10, 10, 10)
+  assert((s.rect.x, s.rect.y, s.rect.height, s.rect.width) == (10,10,10,10))
+  srect = s.getRect()
+  assert(srect.x,  s.getRect().y, srect.height,  srect.width) == (10,10,10,10)
+  assert s.image 
+  print("Test Complete!")
