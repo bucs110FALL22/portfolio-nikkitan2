8272e214d93d5f531f59ffe0b5c9f8ea17931c04
Nikki Tan <112448903+nikkitan2@users.noreply.github.com>
2022-12-13 22:30:23 +0000

    :0

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,76 +1,67 @@
+import pygame
+pygame.init()
+w = 250
+h = 250
+display = pygame.display.set_mode(size=(w, h))
+font = pygame.font.Font(None, 25)
+font_color = "pink"
+font_pos = (15, 15)
+line_color = "pink"
 
-#part a
-def print_3n_plus_1_sequence(n: int) -> None:
-  count = 0
-  while n > 1:
-    count += 1
-    print(n)
-    if n % 2 == 0:
-      n = n // 2
-    else:
-      n = 3 * n + 1
-  print(1)
-  print("The number of iterations was: ", count)
+upper_limit = 20
+iters = {}
+max_so_far = 0
+max_val = 0
+scale = 5
 
+new_display = pygame.transform.flip(display, False, True)
 
-print_3n_plus_1_sequence(101)
-#part b
-def calculate_3n_plus_1_iterations(upper_limit: int) -> None:
-  iters = {}
-  for start in range(2, upper_limit + 1):
-    n = start
-    count = 0
-    while n > 1:
-      count += 1
-      if n % 2 == 0:
-        n = n // 2
-      else:
-        n = 3 * n + 1
-    iters[start] = count
-  print(iters)
+num = []
 
-calculate_3n_plus_1_iterations(10)
-#part c
+stop = upper_limit + 1
+start = 2
 
-def graph():
-  import pygame
-  pygame.init()
-  window = pygame.display.set_mode((500,500))
-  pygame.draw.rect(window, (225,225,225), (0,0,500,500))
-  font = pygame.font.Font(None,20)
-  upper_limit = 20
-  iters = {}
-  max_so_far = 0
-  max_val = 100
-  scale = 5
+coords = []
+iters_c = {}
 
-  for i in range(2,upper_limit):
-    count = 0
-    n = i
-    while n > 1:
-        if n % 2==0:
-            n = n/2
-        else:
-            n = n * 3 + 1
-        count = count + 1
-        iters[n] = count
+def print_3n_plus_1_sequence(n):
+ count = 0
+ while True:
+   if n == 1:
+     break
+   if n % 2 == abs(1):
+     n = 3 * n + 1
+     num.append(n)
+     
+     count +=1
+   
+   elif n % 2 == 0:
+     n = n/2
 
-    iters.items()
-    print(iters[n])
+     num.append(n)
+     count +=1
+ return count
 
-  pygame.draw.lines(window,(0,0,0),False,[(0,250),(500,250)],2)
-  pygame.draw.lines(window,(0,0,0),False,[(250,0),(250,500)],2)
-  pygame.draw.lines(window,(0,0,0),False,[(123,123),(123,123)],2)
-  pygame.draw.lines(window,(0,0,0),False,[(123,123),(123,123)],2)
-  
-  pygame.display.update()
-  pygame.time.wait(5000)
-  new_display = pygame.transform.flip(window,False,True)
-  window.blit(new_display,(0,0))
-  pygame.display.update()
-  pygame.time.wait(5000)
-  msg = font.render("maximum number of iteration:" + str(max_so_far), True, (225,225,225))
-  window.blit(msg,(10,10))
-  pygame.display.update()
-  pygame.time.wait(5000)
-graph()
+for i in range(start, stop):
+  count = print_3n_plus_1_sequence(i)
+  iters[i] = count
+  iters_c[i * scale] = h - count * scale
+  coords = list(iters_c.items())
+
+  if count > max_so_far:
+    max_so_far = count
+    max_val = str(max_so_far)
+    display.blit(new_display, (0,0))
+    msg = font.render("the max value is " + max_val, True, font_color)
+    display.blit(msg, font_pos)
+    pygame.display.flip()
+   
+  if i > start+1:
+   pygame.draw.lines(display, line_color, False, coords)
+   pygame.display.flip()
+
+  pygame.time.wait(500)
+max_val = max_so_far 
+print("iters:", iters)
+
+pygame.time.wait(5000)
